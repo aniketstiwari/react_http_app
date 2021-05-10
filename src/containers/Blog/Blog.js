d7ac5662275602cf2ff468897dd60b260fab3d00
aniketstiwari <anikaittiwari91@gmail.com>
2021-05-10 16:24:07 +0530

    [6] Transform and fetched data

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -16,8 +16,15 @@ class Blog extends Component {
 
         //then() function will execute when the get request will be completed
         axios.get('https://jsonplaceholder.typicode.com/posts').then(response => {
-            console.log(response);
-            this.setState({ posts: response.data });
+            const posts = response.data.slice(0, 4);
+            const updatedPosts = posts.map( post => {
+                return {
+                    ...post, 
+                    author: 'Max'
+                }
+            })
+            //console.log(response);
+            this.setState({ posts: updatedPosts });
         });
         //calling setState() outside of the get function will not set the state
         // because we won't get the whole response as get is a Async request
@@ -26,7 +33,7 @@ class Blog extends Component {
 
     render () {
         const posts = this.state.posts.map( (post) => {
-            return <Post key={post.id} title={post.title} />
+            return <Post key={post.id} title={post.title} author={post.author} />
         })
         return (
             <div>
