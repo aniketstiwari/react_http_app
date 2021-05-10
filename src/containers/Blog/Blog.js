0187f445d630dc1ae6f5b9adf1a2191abd31845c
aniketstiwari <anikaittiwari91@gmail.com>
2021-05-10 16:30:58 +0530

    [7] Making the post selectable

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -8,7 +8,8 @@ import './Blog.css';
 
 class Blog extends Component {
     state = {
-        posts: []
+        posts: [],
+        selectedPostId: null
     }
     componentDidMount() {
         //the below "get" line we be executed asynchronously that's why we can't save
@@ -31,9 +32,17 @@ class Blog extends Component {
 
     }
 
+    postSelectedHandler = (id) => {
+        this.setState({selectedPostId: id})
+    }
+
     render () {
         const posts = this.state.posts.map( (post) => {
-            return <Post key={post.id} title={post.title} author={post.author} />
+            return <Post 
+              key={post.id} 
+              title={post.title} 
+              author={post.author}
+              clicked={() => this.postSelectedHandler(post.id)} />
         })
         return (
             <div>
@@ -41,7 +50,7 @@ class Blog extends Component {
                     {posts}
                 </section>
                 <section>
-                    <FullPost />
+                    <FullPost id={this.state.selectedPostId} />
                 </section>
                 <section>
                     <NewPost />
