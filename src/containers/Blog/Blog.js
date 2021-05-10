bb0596aeccdb2eef86599ba0dcece91d34b78bba
aniketstiwari <anikaittiwari91@gmail.com>
2021-05-10 16:10:25 +0530

    [4] Sending a get request

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 import Post from '../../components/Post/Post';
 import FullPost from '../../components/FullPost/FullPost';
@@ -6,6 +7,17 @@ import NewPost from '../../components/NewPost/NewPost';
 import './Blog.css';
 
 class Blog extends Component {
+    componentDidMount() {
+        //the below "get" line we be executed asynchronously that's why we can't save
+        // in a variable as it is not going to complete immediately
+
+        //then() function will execute when the get request will be completed
+        axios.get('https://jsonplaceholder.typicode.com/posts').then(response => {
+            console.log(response);
+        });
+
+    }
+
     render () {
         return (
             <div>
