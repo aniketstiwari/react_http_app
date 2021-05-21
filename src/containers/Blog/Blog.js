791cf78039ea7803082c50cfc93d8b569bd41a97
aniketstiwari <anikaittiwari91@gmail.com>
2021-05-21 17:38:51 +0530

    Routing [5] preparing the project for routing

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,56 +1,13 @@
 import React, { Component } from 'react';
 //import axios from 'axios';
 import axiosinstance from '../../axios';
+import Posts from './Posts/Posts';
 
 import './Blog.css';
 
 class Blog extends Component {
-    state = {
-        posts: [],
-        selectedPostId: null,
-        error: false
-    }
-    componentDidMount() {
-        //the below "get" line we be executed asynchronously that's why we can't save
-        // in a variable as it is not going to complete immediately
-
-        //then() function will execute when the get request will be completed
-        axiosinstance.get('/posts')
-        .then(response => {
-            const posts = response.data.slice(0, 4);
-            const updatedPosts = posts.map( post => {
-                return {
-                    ...post, 
-                    author: 'Max'
-                }
-            })
-            //console.log(response);
-            this.setState({ posts: updatedPosts });
-        })
-        .catch(error => {
-            //console.log(error);
-            this.setState({error: true})
-        });
-        //calling setState() outside of the get function will not set the state
-        // because we won't get the whole response as get is a Async request
-
-    }
-
-    postSelectedHandler = (id) => {
-        this.setState({selectedPostId: id})
-    }
-
     render () {
-        let posts = <p style={{textAlign: 'center'}}>Something went wrong</p>
-        if(!this.state.error){
-            posts = this.state.posts.map( (post) => {
-                return <Post 
-                  key={post.id} 
-                  title={post.title} 
-                  author={post.author}
-                  clicked={() => this.postSelectedHandler(post.id)} />
-            });
-        }
+        
         return (
             <div className="Blog">
                 <header>
@@ -61,10 +18,7 @@ class Blog extends Component {
                         </ul>
                     </nav>
                 </header>
-                <section className="Posts">
-                    {posts}
-                </section>
-                
+                <Posts />
             </div>
         );
     }
---
+++ src/containers/Blog/Posts/Posts.js
@@ -0,0 +1,60 @@
+import React, { Component } from "react";
+import axiosinstance from '../../../axios';
+import Post from '../../../components/Post/Post';
+import './Posts.css';
+
+class Posts extends Component {
+    state = {
+        posts: []
+    }
+
+    componentDidMount() {
+        //the below "get" line we be executed asynchronously that's why we can't save
+        // in a variable as it is not going to complete immediately
+
+        //then() function will execute when the get request will be completed
+        axiosinstance.get('/posts')
+        .then(response => {
+            const posts = response.data.slice(0, 4);
+            const updatedPosts = posts.map( post => {
+                return {
+                    ...post, 
+                    author: 'Max'
+                }
+            })
+            //console.log(response);
+            this.setState({ posts: updatedPosts });
+        })
+        .catch(error => {
+            //console.log(error);
+            //this.setState({error: true})
+        });
+        //calling setState() outside of the get function will not set the state
+        // because we won't get the whole response as get is a Async request
+
+    }
+
+    postSelectedHandler = (id) => {
+        this.setState({selectedPostId: id})
+    }
+
+    render() {
+        let posts = <p style={{textAlign: 'center'}}>Something went wrong</p>
+        if(!this.state.error){
+            posts = this.state.posts.map( (post) => {
+                return <Post 
+                  key={post.id} 
+                  title={post.title} 
+                  author={post.author}
+                  clicked={() => this.postSelectedHandler(post.id)} />
+            });
+        }
+        return (
+            <section className="Posts">
+                {posts}
+            </section>
+        )
+    }
+}
+
+export default Posts;
