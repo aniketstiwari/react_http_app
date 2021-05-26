3ab7bbb6b4e647bf8bcc713356425ebc1ba1fe0d
aniketstiwari <anikaittiwari91@gmail.com>
2021-05-26 10:24:44 +0530

    routing [9] using links

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 //import axios from 'axios';
 // import axiosinstance from '../../axios';
-import { Route } from 'react-router-dom';
+import { Route, Link } from 'react-router-dom';
 
 import './Blog.css';
 import Posts from './Posts/Posts';
@@ -14,8 +14,18 @@ class Blog extends Component {
                 <header>
                     <nav>
                         <ul>
-                            <li><a href="/">Home</a></li>
-                            <li><a href="new-post">New Post</a></li>
+                            {/** Link will create an anchor tag and will prevent the
+                             * default behvaiour of sending a new request
+                             */}
+                            <li><Link to="/">Home</Link></li>
+                            {/** hash: property will allow us to then jump to any
+                             * id element we specified
+                             */}
+                            <li><Link to={{
+                                pathname: "/new-post",
+                                hash: '#submit',
+                                search: '?quick-submit=true' 
+                            }}>New Post</Link></li>
                         </ul>
                     </nav>
                 </header>
@@ -29,6 +39,11 @@ class Blog extends Component {
                 {/** We can use as many routes on the same page */}
                 {/* <Route path="/" exact render={ () => <h1>Home 2</h1> } /> */}
 
+                {/**There is a issue here whenever we are clicking on any click
+                 * we are reloading the page which will load the whole dom we will lose
+                 * state. Instead we need to render the components whenever user clicks
+                 * on any link
+                 */}
                 <Route path="/" exact component={Posts} />
                 <Route path="/new-post" component={NewPost} />
             </div>
