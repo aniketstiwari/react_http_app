e87e26ed4728fe1dc180f4d67e29534b3f2d7f1a
aniketstiwari <anikaittiwari91@gmail.com>
2021-05-26 10:10:01 +0530

    Routing [6] setting up and rendering routes

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,13 +1,12 @@
 import React, { Component } from 'react';
 //import axios from 'axios';
-import axiosinstance from '../../axios';
-import Posts from './Posts/Posts';
+// import axiosinstance from '../../axios';
+import { Route } from 'react-router-dom';
 
 import './Blog.css';
 
 class Blog extends Component {
     render () {
-        
         return (
             <div className="Blog">
                 <header>
@@ -18,7 +17,15 @@ class Blog extends Component {
                         </ul>
                     </nav>
                 </header>
-                <Posts />
+                {/** Route needs some configuration like path property */}
+                {/** Render holds a reference to a function. We can even add anonymous
+                 * function */}
+                 {/** Right it match the prefix from the path that's why we see
+                  * <h1>Home</h1> in both the url. We can override this behavior by using
+                  * exact prop. exact prop is a boolean prop */}
+                <Route path="/" exact render={ () => <h1>Home</h1> } />
+                {/** We can use as many routes on the same page */}
+                <Route path="/" exact render={ () => <h1>Home 2</h1> } />
             </div>
         );
     }
