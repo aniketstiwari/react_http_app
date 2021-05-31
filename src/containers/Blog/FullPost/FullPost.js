70de47fae4f3ef0a0a96522d7213605f02bbec16
aniketstiwari <anikaittiwari91@gmail.com>
2021-05-31 17:29:04 +0530

    Routing [20] diving deeper into nested routes

--- src/containers/Blog/FullPost/FullPost.js
+++ src/containers/Blog/FullPost/FullPost.js
@@ -11,9 +11,20 @@ class FullPost extends Component {
         };
     }
     componentDidMount() {
-        console.log(this.props)
+        console.log(this.props);
+        this.loadData();
+    }
+
+    componentDidUpdate() {
+        this.loadData();
+    }
+
+    loadData() {
         if(this.props.match.params.id){
-            if(!this.state.loadedPost || (this.state.loadedPost && this.state.loadedPost.id !== this.props.id)){
+            //this..match.params.id will be string and this.state.loadedPost.id is integer
+            // so inorder to compare we need to change our inequality to !=
+            //otherwise we can add plus(+) infront of this.props.match.params.id
+            if(!this.state.loadedPost || (this.state.loadedPost && this.state.loadedPost.id !== +this.props.match.params.id)){
                 axios.get('/posts/' + this.props.match.params.id)
                 .then(response => {
                     //console.log(response);
@@ -23,11 +34,11 @@ class FullPost extends Component {
                     this.setState({loadedPost: response.data});
                 });
             }
-        }        
+        }
     }
 
     deletePostHandler = () => {
-        axios.delete('/posts/' + this.props.id)
+        axios.delete('/posts/' + this.props.match.params.id)
         .then(response => {
             console.log(response);
         });
@@ -35,7 +46,7 @@ class FullPost extends Component {
 
     render () {
         let post = <p style={{textAlign: 'center'}}>Please select a Post!</p>;
-        if(this.props.id){
+        if(this.props.match.params.id){
             post = <p style={{textAlign: 'center'}}>Loading</p>;
         }
         if(this.state.loadedPost) {
